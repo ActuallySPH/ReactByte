8ae2736bbbabfa98861e96416611c92ffcff114d
suraj Puthran <sputhran98@gmail.com>
2020-06-29 11:30:39 +0530

    first commit

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Likes from "./common/likes";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
   };
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
   };
+
+  handleViewMore = (movie) => {
+    console.log("Clicked on view more: " + movie);
+  };
+
+  handleLike(movie) {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+    console.log(movie);
+  }
 
   render() {
     const { length: count } = this.state.movies;
@@ -19,29 +33,45 @@ class Movies extends Component {
     return (
       <React.Fragment>
         <p>Showing {count} movies in the database.</p>
-        <table className="table">
+        <table className="table table-sm">
           <thead>
             <tr>
               <th>Title</th>
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th>Like</th>
+              <th>Action</th>
               <th />
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {this.state.movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
+                  <Likes
+                    onClick={() => this.handleLike(movie)}
+                    liked={movie.liked}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleViewMOre(movie)}
+                    className="btn btn-primary btn-sm"
+                  >
+                    View More
+                  </button>
+                </td>
+                <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
-                    className="btn btn-danger btn-sm"
+                    className="btn btn-warning btn-sm"
                   >
-                    Delete
+                    x
                   </button>
                 </td>
               </tr>
